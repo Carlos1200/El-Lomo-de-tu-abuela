64f26746bc3bcb9ec87b40c056206967480bb3e9
Carlos1200 <50189393+Carlos1200@users.noreply.github.com>
2020-10-30 15:41:58 -0600

    Menu completo y localStorage

--- Restaurante/js/main.js
+++ Restaurante/js/main.js
@@ -1,14 +1,13 @@
-var nombre = document.querySelector('#name');
-var email = document.querySelector('input[type="email"]');
-var telefono = document.querySelector('#telefono');
-var errorDiv = document.getElementById('error');
-var hora = document.querySelector('#hora');
-var reserva = document.querySelector('#reservacion');
-var area = document.querySelector('#areas');
-var inputFecha = document.querySelector('input[type="date"]');
-
-
-
+const nombre = document.querySelector('#name');
+const email = document.querySelector('input[type="email"]');
+const telefono = document.querySelector('#telefono');
+const errorDiv = document.getElementById('error');
+const hora = document.querySelector('#hora');
+const reserva = document.querySelector('#reservacion');
+const area = document.querySelector('#areas');
+const inputFecha = document.querySelector('input[type="date"]');
+let estadoArea = [];
+let estadoMesa = [];
 
 $(function() {
     $('.tooltip-carousel').popover();
@@ -32,28 +31,58 @@ $(function() {
     }).mouseleave(function() {
         $(this).popover('hide');
     });
+
 });
 
-//EVENT LISTEDERS
 
+
+//EVENT LISTEDERS
+//eventos para reservar mesa
 if (filename() === "reservacion-mesa.html") {
     //Cambiar color de reservacion
     reserva.addEventListener('click', cambiarEstado);
 
     //Cambiar color de registros seleccionados
     document.querySelector('#registrado').addEventListener('click', reservarMesa);
-}
 
+    //Aumentar o disminuir Zoom
+    document.querySelector('#zoom').addEventListener('click', cambiarZoom);
+
+    //Cargar LocalStorage
+    document.addEventListener('DOMContentLoaded', () => {
+        estadoMesa = JSON.parse(localStorage.getItem('mesa')) || [];
+
+
+        sincronizarMesa();
+    });
+}
+//eventos para reservar area
 if (filename() === "reservacion-area.html") {
     //Cambiar color de Areas
     area.addEventListener('click', cambiarEstadoArea);
 
     //Cambiar color de registros seleccionados
     document.querySelector('#registrado').addEventListener('click', reservarArea);
+
+    //Aumentar o disminuir Zoom
+    document.querySelector('#zoom').addEventListener('click', cambiarZoom);
+
+    //Cargar LocalStorage
+    document.addEventListener('DOMContentLoaded', () => {
+        estadoArea = JSON.parse(localStorage.getItem('area')) || [];
+
+
+        sincronizarArea();
+    });
 }
 
-//Aumentar o disminuir Zoom
-document.querySelector('#zoom').addEventListener('click', cambiarZoom);
+
+if (filename() === "Menu.html") {
+    document.addEventListener('DOMContentLoaded', () => {
+        obtenerDatos();
+    });
+}
+
 
 
 //Nombre de la pagina
@@ -64,8 +93,58 @@ function filename() {
     return rutaRelativa;
 }
 
+function obtenerDatos() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+    const urlB = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink';
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarHTML(resultado));
 
+    fetch(urlB)
+        .then(resultado => resultado.json())
+        .then(datos => mostrarHTMLBebidas(datos));
+}
 
+function mostrarHTML(datos) {
+    const contenido = document.querySelector('div .menu');
+
+    let html = '';
+    const { meals } = datos;
+
+    meals.forEach(platillo => {
+        const { strMeal, strMealThumb } = platillo;
+
+        html += `
+            <div class="col-md-4 p-3">
+                <img src="${strMealThumb}" class="img-thumbnail">
+                <p class="d-block text-white text-center font-weight-bold" style="font-size: 1.5rem;">${strMeal}</p>
+            </div>
+        
+        `;
+    });
+
+    contenido.innerHTML = html;
+}
+
+function mostrarHTMLBebidas(datos) {
+    const contenido = document.querySelector('div .bebidas');
+
+    let html = '';
+    const { drinks } = datos;
+    drinks.forEach(bebida => {
+        const { strDrink, strDrinkThumb } = bebida;
+
+        html += `
+            <div class="col-md-4 p-3">
+                <img src="${strDrinkThumb}" class="img-thumbnail">
+                <p class="d-block text-white text-center font-weight-bold" style="font-size: 1.5rem;">${strDrink}</p>
+            </div>
+        
+        `;
+    });
+
+    contenido.innerHTML = html;
+}
 
 //Cambiar color de Reservacion
 function cambiarEstado(e) {
@@ -119,11 +198,25 @@ function cambiarEstadoArea(e) {
     }
 }
 
+function verificarEstado(divArea) {
+    let contador = 0;
+    Array.from(divArea.children).forEach(area => {
+        if (area.classList.contains('activo')) {
+            contador++;
+        }
+    });
+    if (contador > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function reservarMesa(e) {
 
     e.preventDefault();
 
-    if (validarCampos() && validarFecha()) {
+    if (validarCampos() && validarFecha() && verificarEstado(reserva)) {
         Swal.fire({
             title: 'Seguro(a)?',
             text: "Usted va a reservar la mesa",
@@ -140,10 +233,14 @@ function reservarMesa(e) {
 
                 for (var x = 0; x < reserva.children.length; x++) {
                     if (reserva.children[x].classList.contains('activo')) {
+                        let id = reserva.children[x].getAttribute('data-id');
                         reserva.children[x].classList.remove('activo');
                         reserva.children[x].classList.add('reservado');
                         reserva.children[x].children[0].classList.remove('reserva');
                         reserva.children[x].children[0].classList.add('ocupado');
+
+                        estadoMesa = [...estadoMesa, [id, 'activo', 'reservado', 'reserva', 'ocupado']];
+                        sincronizarStorage('mesa', estadoMesa);
                     }
                 }
 
@@ -161,7 +258,7 @@ function reservarMesa(e) {
 
 function reservarArea(e) {
     e.preventDefault();
-    if (validarCampos() && validarFecha()) {
+    if (validarCampos() && validarFecha() && verificarEstado(area)) {
 
 
         Swal.fire({
@@ -178,14 +275,17 @@ function reservarArea(e) {
 
                 for (var x = 0; x < area.children.length; x++) {
                     if (area.children[x].classList.contains('activo')) {
+                        let id = area.children[x].getAttribute('data-id');
                         area.children[x].classList.remove('fa-door-open');
                         area.children[x].classList.remove('activo');
                         area.children[x].classList.add('reservado');
                         area.children[x].classList.add('fa-door-closed');
                         area.children[x].children[0].classList.remove('reserva');
                         area.children[x].children[0].classList.add('ocupado');
+                        estadoArea = [...estadoArea, [id, 'fa-door-open', 'fa-door-closed', 'activo', 'reservado', 'reserva', 'ocupado']];
+                        sincronizarStorage('area', estadoArea);
                     }
-                }
+                };
 
                 Swal.fire(
                     '¡Reservado!',
@@ -193,7 +293,7 @@ function reservarArea(e) {
                     'success'
                 )
             }
-        })
+        });
     }
 
 }
@@ -230,8 +330,6 @@ function validarCampos() {
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     //Validar telefono
     var phoneno = /^[0-9]{8}$/i;
-    console.log(emailRegex.test(email.value));
-    console.log(telefono.value.match(phoneno));
     if (emailRegex.test(email.value) && telefono.value.match(phoneno) && nombre.value.length != 0 && parseInt(hora.value.substring(0, 2)) >= 12 && parseInt(hora.value.substring(0, 2)) <= 20) {
 
         return true;
@@ -255,7 +353,6 @@ function cambiarZoom(e) {
         var escala = parseFloat(elemento.getPropertyValue('scale')).toFixed(1);
 
         if (parseFloat(escala) < 1.5) {
-            console.log(escala);
             zoom.style.scale = parseFloat(escala) + 0.1;
         }
     }
@@ -266,9 +363,45 @@ function cambiarZoom(e) {
         var escala = parseFloat(elemento.getPropertyValue('scale')).toFixed(1);
 
         if (parseFloat(escala) > 1) {
-            console.log(escala);
             zoom.style.scale = parseFloat(escala) - 0.1;
         }
     }
 
+}
+
+function sincronizarStorage(tipo, estado) {
+    localStorage.setItem(tipo, JSON.stringify(estado));
+}
+
+function sincronizarArea() {
+    let iconos = Array.from(area.children);
+    if (estadoArea) {
+        estadoArea.forEach(estado => {
+            iconos.find(icono => {
+                if (icono.getAttribute('data-id') === estado[0]) {
+                    icono.classList.remove(estado[1]);
+                    icono.classList.remove(estado[3]);
+                    icono.children[0].classList.remove(estado[5]);
+                    icono.classList.add(estado[2]);
+                    icono.classList.add(estado[4]);
+                    icono.children[0].classList.add(estado[6]);
+                }
+            });
+        });
+    }
+
+}
+
+function sincronizarMesa() {
+    let iconos = Array.from(reserva.children);
+    estadoMesa.forEach(estado => {
+        iconos.forEach(icono => {
+            if (icono.getAttribute('data-id') === estado[0]) {
+                icono.classList.remove(estado[1]);
+                icono.children[0].classList.remove(estado[3]);
+                icono.classList.add(estado[2]);
+                icono.children[0].classList.add(estado[4]);
+            }
+        });
+    });
 }
