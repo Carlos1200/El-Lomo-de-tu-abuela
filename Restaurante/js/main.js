bfcde847576005122e243e7e1adda6a4956c4251
Carlos1200 <50189393+Carlos1200@users.noreply.github.com>
2020-11-02 23:38:12 -0600

    AdminApiRest

--- Restaurante/js/main.js
+++ Restaurante/js/main.js
@@ -6,6 +6,10 @@ const hora = document.querySelector('#hora');
 const reserva = document.querySelector('#reservacion');
 const area = document.querySelector('#areas');
 const inputFecha = document.querySelector('input[type="date"]');
+const addplatillo = document.querySelector('.platillo');
+const addbebida = document.querySelector('.bebida');
+const contenidoplatillo = document.querySelector('.adminPlatillos');
+const contenidobebida = document.querySelector('.adminBebidas');
 let estadoArea = [];
 let estadoMesa = [];
 
@@ -83,6 +87,28 @@ if (filename() === "Menu.html") {
     });
 }
 
+if (filename() === "admin.html") {
+    document.addEventListener('DOMContentLoaded', () => {
+        obtenerDatos();
+    });
+    addplatillo.addEventListener('click', () => {
+        window.location.href = 'formulario.html?id=0&t=platillo';
+    });
+    addbebida.addEventListener('click', () => {
+        window.location.href = 'formulario.html?id=0&t=bebida';
+    });
+}
+let archivo = filename();
+if (archivo.includes("formulario.html")) {
+    document.addEventListener('DOMContentLoaded', () => {
+        obtenerDatosForm();
+    });
+    document.querySelector('input[name="url"]').addEventListener('blur', () => {
+        document.querySelector('#imgForm').src = document.querySelector('input[name="url"]').value;
+    });
+    document.querySelector('.btn-lg').addEventListener('click', enviarForm);
+
+}
 
 
 //Nombre de la pagina
@@ -94,30 +120,63 @@ function filename() {
 }
 
 function obtenerDatos() {
-    const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
-    const urlB = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink';
-    fetch(url)
+    const url = 'https://restauranteappudb.herokuapp.com/api/menu';
+    const urlB = 'https://restauranteappudb.herokuapp.com/api/bebida';
+    fetch(url, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Access-Control-Allow-Origin': '*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive'
+            }
+        })
         .then(respuesta => respuesta.json())
-        .then(resultado => mostrarHTML(resultado));
+        .then(resultado => {
+            if (filename() === "admin.html") {
+                return mostrarAdmin(resultado);
+            } else if (filename() === 'Menu.html') {
+                return mostrarHTML(resultado);
+            }
 
-    fetch(urlB)
-        .then(resultado => resultado.json())
-        .then(datos => mostrarHTMLBebidas(datos));
+        });
+    fetch(urlB, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Access-Control-Allow-Origin': '*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive'
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            if (filename() === "admin.html") {
+                return mostrarAdminBebidas(resultado);
+            } else if (filename() === 'Menu.html') {
+                return mostrarHTMLBebidas(resultado);
+            }
+
+        });
 }
 
 function mostrarHTML(datos) {
     const contenido = document.querySelector('div .menu');
-
     let html = '';
-    const { meals } = datos;
 
-    meals.forEach(platillo => {
-        const { strMeal, strMealThumb } = platillo;
+    datos.forEach(platillo => {
+        const { name, description, price, image } = platillo;
 
         html += `
-            <div class="col-md-4 p-3">
-                <img src="${strMealThumb}" class="img-thumbnail">
-                <p class="d-block text-white text-center font-weight-bold" style="font-size: 1.5rem;">${strMeal}</p>
+            <div class="col-md-6 p-3">
+                <img src="${image}" class="img-thumbnail">
+                <p class="d-block text-white text-center font-weight-bold mb-0" style="font-size: 3rem;">${name}</p>
+                <p class="d-block text-white text-center mb-0" style="font-size: 1.5rem;">Precio: $<span class="font-weight-bold">${price}</span></p>
+                <p class="d-block text-white text-center  mb-0" style="font-size: 1.5rem;">${description}</p>
             </div>
         
         `;
@@ -126,24 +185,124 @@ function mostrarHTML(datos) {
     contenido.innerHTML = html;
 }
 
-function mostrarHTMLBebidas(datos) {
-    const contenido = document.querySelector('div .bebidas');
-
+function mostrarAdmin(datos) {
     let html = '';
-    const { drinks } = datos;
-    drinks.forEach(bebida => {
-        const { strDrink, strDrinkThumb } = bebida;
+    limpiarHTML();
+
+    datos.forEach(platillo => {
+        const { name, image, id } = platillo;
+        //Crear Elementos
+        const btnEdit = document.createElement('button');
+        const btndelete = document.createElement('button');
+        const icoDelete = document.createElement('i');
+        const icoEdit = document.createElement('i');
+        const divContenido = document.createElement('div');
+        const img = document.createElement('img');
+        const nombre = document.createElement('h3');
+        const divButtons = document.createElement('div');
+
+        //asignar datos
+        btndelete.classList.add('btn', 'btn-danger');
+        icoDelete.classList.add('fas', 'fa-trash', 'py-2');
+        btndelete.setAttribute('data-food', id);
+        divButtons.classList.add('d-md-flex', 'justify-content-center');
+        nombre.classList.add('text-white', 'text-center', 'mr-3', 'd-block');
+        nombre.textContent = name;
+        img.classList.add('img-thumbnail');
+        img.src = image;
+        divContenido.classList.add('col-md-4');
+        btnEdit.classList.add('btn', 'btn-warning', 'mr-2');
+        icoEdit.classList.add('fas', 'fa-edit', 'py-2');
+        btnEdit.setAttribute('data-food', id);
+
+        //unirlos
+        btnEdit.appendChild(icoEdit);
+        btndelete.appendChild(icoDelete);
+        divButtons.appendChild(btnEdit);
+        divButtons.appendChild(btndelete);
+        divContenido.appendChild(img);
+        divContenido.appendChild(nombre);
+        divContenido.appendChild(divButtons);
+        contenidoplatillo.appendChild(divContenido);
+
+        //Agregar funciones
+        btnEdit.onclick = () => {
+            EditarPlatillo(id);
+        }
+        btndelete.onclick = () => {
+            borrarPlatillo(id);
+        }
+    });
+
+}
+
+function mostrarAdminBebidas(datos) {
+    let html = '';
+    limpiarHTMLBebidas();
+    datos.forEach(bebida => {
+        const { name, image, id } = bebida;
+        //Crear Elementos
+        const btnEdit = document.createElement('button');
+        const btndelete = document.createElement('button');
+        const icoDelete = document.createElement('i');
+        const icoEdit = document.createElement('i');
+        const divContenido = document.createElement('div');
+        const img = document.createElement('img');
+        const nombre = document.createElement('h3');
+        const divButtons = document.createElement('div');
+
+        //asignar datos
+        btndelete.classList.add('btn', 'btn-danger');
+        icoDelete.classList.add('fas', 'fa-trash', 'py-2');
+        btndelete.setAttribute('data-drink', id);
+        divButtons.classList.add('d-md-flex', 'justify-content-center');
+        nombre.classList.add('text-white', 'text-center', 'mr-3', 'd-block');
+        nombre.textContent = name;
+        img.classList.add('img-thumbnail');
+        img.src = image;
+        divContenido.classList.add('col-md-4');
+        btnEdit.classList.add('btn', 'btn-warning', 'mr-2');
+        icoEdit.classList.add('fas', 'fa-edit', 'py-2');
+        btnEdit.setAttribute('data-drink', id);
+
+        //unirlos
+        btnEdit.appendChild(icoEdit);
+        btndelete.appendChild(icoDelete);
+        divButtons.appendChild(btnEdit);
+        divButtons.appendChild(btndelete);
+        divContenido.appendChild(img);
+        divContenido.appendChild(nombre);
+        divContenido.appendChild(divButtons);
+        contenidobebida.appendChild(divContenido);
+
+        //Agregar funciones
+        btnEdit.onclick = () => {
+            EditarBebida(id);
+        }
+        btndelete.onclick = () => {
+            borrarBebida(id);
+        }
+    });
+}
+
+function mostrarHTMLBebidas(datos) {
+    const contenidoBeb = document.querySelector('div .bebidas');
+    let html = '';
+    datos.forEach(bebida => {
+        const { name, description, price, image } = bebida;
 
         html += `
-            <div class="col-md-4 p-3">
-                <img src="${strDrinkThumb}" class="img-thumbnail">
-                <p class="d-block text-white text-center font-weight-bold" style="font-size: 1.5rem;">${strDrink}</p>
-            </div>
+        <div class="col-md-6 p-3">
+            <img src="${image}" class="img-thumbnail">
+            <p class="d-block text-white text-center font-weight-bold mb-0" style="font-size: 3rem;">${name}</p>
+            <p class="d-block text-white text-center mb-0" style="font-size: 1.5rem;">Precio: $<span class="font-weight-bold">${price}</span></p>
+            <p class="d-block text-white text-center  mb-0" style="font-size: 1.5rem;">${description}</p>
+        </div>
         
         `;
     });
 
-    contenido.innerHTML = html;
+    contenidoBeb.innerHTML = html;
 }
 
 //Cambiar color de Reservacion
@@ -404,4 +563,168 @@ function sincronizarMesa() {
             }
         });
     });
+}
+
+function EditarPlatillo(id) {
+    window.location.href = `formulario.html?id=${id}&t=${"platillo"}`;
+
+}
+
+function EditarBebida(id) {
+    window.location.href = `formulario.html?id=${id}&t=${"bebida"}`;
+}
+
+function borrarBebida(id) {
+    url = 'https://restauranteappudb.herokuapp.com/api/bebida/' + id;
+    fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Access-Control-Allow-Origin': '*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive'
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarAdminBebidas(resultado));
+}
+
+function borrarPlatillo(id) {
+    url = 'https://restauranteappudb.herokuapp.com/api/menu/' + id;
+    fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Access-Control-Allow-Origin': '*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive'
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarAdmin(resultado));
+}
+
+function obtenerDatosForm() {
+    let valores = window.location.search;
+    let id = valores.substring(4, 5);
+    let tipo = valores.substring(8);
+    if (id > 0) {
+        if (tipo === "platillo") {
+            var url = 'https://restauranteappudb.herokuapp.com/api/menu/' + id;
+        } else if (tipo === "bebida") {
+            var url = 'https://restauranteappudb.herokuapp.com/api/bebida/' + id;
+        }
+        fetch(url, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Connection': 'keep-alive'
+                }
+            })
+            .then(respuesta => respuesta.json())
+            .then(resultado => llenarFormulario(resultado));
+    }
+
+}
+
+
+function llenarFormulario(datos) {
+    const nombre = document.querySelector('input[name="name"]');
+    const descripti = document.querySelector('input[name="descrip"]');
+    const precio = document.querySelector('input[name="price"]');
+    const url = document.querySelector('input[name="url"]');
+    const imagen = document.querySelector('#imgForm');
+    datos.forEach(platillo => {
+        const { name, description, price, image } = platillo;
+        nombre.value = name;
+        descripti.value = description;
+        precio.value = price;
+        url.value = image;
+        imagen.src = platillo.image;
+    });
+}
+
+function enviarForm(e) {
+    e.preventDefault();
+    const nombre = document.querySelector('input[name="name"]');
+    const descripti = document.querySelector('input[name="descrip"]');
+    const precio = document.querySelector('input[name="price"]');
+    const url = document.querySelector('input[name="url"]');
+
+    let valores = window.location.search;
+    let id = valores.substring(4, 5);
+    let tipo = valores.substring(8);
+    console.log(tipo);
+    const json = {
+        name: nombre.value,
+        description: descripti.value,
+        price: precio.value,
+        image: url.value
+    }
+    if (id > 0) {
+        if (tipo === "platillo") {
+            var direc = 'https://restauranteappudb.herokuapp.com/api/menu/' + id;
+        } else {
+            var direc = 'https://restauranteappudb.herokuapp.com/api/bebida/' + id;
+        }
+        fetch(direc, {
+                method: 'PUT',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Connection': 'keep-alive'
+                },
+                body: JSON.stringify(json)
+            })
+            .then(function() {
+                window.location.href = `admin.html`;
+            });
+
+    } else {
+        if (tipo === "platillo") {
+            var direc = 'https://restauranteappudb.herokuapp.com/api/menu';
+        } else {
+            var direc = 'https://restauranteappudb.herokuapp.com/api/bebida';
+        }
+        fetch(direc, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Connection': 'keep-alive'
+                },
+                body: JSON.stringify(json)
+            })
+            .then(function() {
+                window.location.href = `admin.html`;
+            });
+    }
+
+}
+
+function limpiarHTML() {
+
+
+    while (contenidoplatillo.firstChild) {
+        contenidoplatillo.removeChild(contenidoplatillo.firstChild);
+    }
+}
+
+function limpiarHTMLBebidas() {
+
+    while (contenidobebida.firstChild) {
+        contenidobebida.removeChild(contenidobebida.firstChild);
+    }
 }
